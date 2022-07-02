f25e8b558e9ec21eeef105063328736570cb50e0
Sarah Zhang <15064710+sazhang@users.noreply.github.com>
2022-07-01 22:33:26 -0700

    apply layout to members pg

--- pages/members.js
+++ pages/members.js
@@ -1,16 +1,46 @@
+import { useEffect, useState } from "react";
+import Head from "next/head";
 import MemberCard from "../components/memberCard";
 import { Members } from "../components/membersData";
+import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Member = () => {
+  const [preferedColorScheme, setPreferedColorScheme] = useState("light");
+
+  useEffect(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setPreferedColorScheme("dark");
+    }
+  }, []);
+
   return (
-    <div className="bg-white">
+    <Layout>
+      <Head>
+        <title>About | Women Build Web3</title>
+        <meta
+          name="description"
+          content="Providing education, oppportunities, and funding to a new wave of web3 builders"
+        />
+        {preferedColorScheme === "light" && (
+          <link rel="icon" href="/favicon.ico" />
+        )}
+        {preferedColorScheme === "dark" && (
+          <link rel="icon" href="/favicon-white.ico" />
+        )}
+      </Head>
+      <Navbar preferedColorScheme={preferedColorScheme} />
       <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
         <div className="space-y-12">
           <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
             <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
               Our Team
             </h2>
-            <p className="text-xl text-gray-500">
+            <p className="text-xl text-gray-200">
               We are a community of women and non-binary developers in web3.
             </p>
           </div>
@@ -30,7 +60,8 @@ const Member = () => {
           </ul>
         </div>
       </div>
-    </div>
+      <Footer preferedColorScheme={preferedColorScheme} />
+    </Layout>
   );
 };
 
